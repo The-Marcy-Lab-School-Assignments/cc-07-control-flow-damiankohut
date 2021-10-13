5b4b02e2c67091374a6c0e52ccb9a5fb17c924f8
damian <damiankohut@gmail.com>
2021-10-13 14:59:15 +0000

    answers to questions

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 //1
 function sumOfFourAndSix(){
-  
-}
+  let sum = 0;
+  for (let i = 0; i <= 1000;i++){
+    if(i % 4 === 0 && i % 6 === 0){
+        sum +=i
+    }
+  }
+} return sum;
+console.log(sumOfFourAndSix());
 
 //2
 function oddAndEvenInFifteen(){
-  
+  for (let i = 0; i <= 15; i++) {
+    if (i % 2 === 0){
+        console.log(i+ "is even"){
+        }
+        else{
+            console.log(i+ "is odd")
+        }
+        }
+
+}console.log(oddAndEvenInFifteen())
 }
+
 
 //3
-function assignGrade(){
-  
+function assignGrade(score){
+if (score > 90)
+return "A"
 }
-
-
+else if (score > 80){
+    return "B"
+} else if ( score >70){
+    return "C"
+} else if (score >= 65){
+    return "D"
+}   else {
+    return "F"
+}
+assignGrade(80)
 
 
 
